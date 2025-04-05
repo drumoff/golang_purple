00bee7634c14dbfd6ce9f8fa72296c1eff9fbf44
drumoff <drumoff@mail.ru>
2025-04-05 19:47:26 +0300

    3.5 lesson

--- go-demo/main.go
+++ go-demo/main.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
 	var userHeight = 1.8
-	var userKg = 100
-	var IMT = userKg / userHeight
+	var userKg float64 = 100
+	var IMT = userKg / math.Pow(userHeight, 2)
+	fmt.Print(IMT) 
 }

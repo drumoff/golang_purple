1120ac45bf484dd7ead652869528a84c804037e8
drumoff <drumoff@mail.ru>
2025-04-07 17:35:30 +0300

    4.1 lesson

--- go-demo/main.go
+++ go-demo/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("Привет")
+	const IMTPower float64 = 2
+	var userHeight float64
+	var userKg float64
+	fmt.Println("Введите свой рост в метрах")
+	fmt.Scan(&userHeight)
+	fmt.Println("Введите свой вес в киллограммах")
+	fmt.Scan(&userKg)
+	IMT := userKg / math.Pow(userHeight, IMTPower)
+	fmt.Print("Индекс массы тела: ")
+	fmt.Print(IMT)
 }

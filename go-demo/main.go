73559cc8b24a8b406f1eedc08d61d728e98dd311
drumoff <drumoff@mail.ru>
2025-04-10 17:42:33 +0300

    4.5 lesson

--- go-demo/main.go
+++ go-demo/main.go
@@ -9,12 +9,19 @@ func main() {
 	const IMTPower float64 = 2
 	var userHeight float64
 	var userKg float64
-	fmt.Print(`___ Калькулятор индекса массы тела ___`) // Мультистроки
+	fmt.Print(`
+___ Калькулятор индекса массы тела ___
+--------------------------------------	
+`) // Мультистроки
 	fmt.Println("Введите свой рост в сантиметрах")
 	fmt.Scan(&userHeight)
 	fmt.Println("Введите свой вес в киллограммах")
 	fmt.Scan(&userKg)
-	IMT := userKg / math.Pow(userHeight / 100, IMTPower)
-	result := fmt.Sprintf("Индекс массы тела: %.0f", IMT)
-	fmt.Print(result)  
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	outputResult(IMT)
+}
+
+func outputResult(imt float64) {
+	result := fmt.Sprintf("Индекс массы тела: %.0f", imt)
+	fmt.Print(result)
 }

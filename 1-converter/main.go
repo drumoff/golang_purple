c594529d4fe4772d442182315fe89464dd04f6c4
drumoff <drumoff@mail.ru>
2025-04-11 18:08:26 +0300

    4.11 lesson

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
+const USDtoEUR float64 = 1
+const USDtoRUB float64 = 3
+const EURtoRUB float64 = USDtoRUB / USDtoEUR
+
 func main() {
-	const USDtoEUR float64 = 1
-	const USDtoRUB float64 = 3
-	const EURtoRUB float64 =  USDtoRUB / USDtoEUR
+	returnInputUser()
+}
+
+func returnInputUser() string {
+	var input string
+	fmt.Scan(&input)
+	return input
+}
+
+func calcMoney(summ int, firstСurrency string, secCurrency string) float64 {
+	var newSumm float64
+	fmt.Println(summ)
+	return newSumm
 }

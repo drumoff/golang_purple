d94d7c6457ac3c27cebf41b419dd7bce197f5a72
drumoff <drumoff@mail.ru>
2025-04-12 12:08:56 +0300

    lesson 5.8

--- go-demo/main.go
+++ go-demo/main.go
@@ -8,32 +8,36 @@ import (
 const IMTPower float64 = 2
 
 func main() {
-
 	fmt.Print(`
 ___ Калькулятор индекса массы тела ___
 --------------------------------------	
-`) // Мультистроки
-	usrHeight, usrKg := getUserInput()
-	IMT := calculateIMT(usrKg, usrHeight)
-
-	switch {
-	case IMT < 16:
-		fmt.Println("У вас сильный дефицит массы тела!")
-	case IMT < 18.5:
-		fmt.Println("У вас дефицит массы тела!")
-	case IMT < 25:
-		fmt.Println("У вас нормальный вес!")
-	case IMT < 30:
-		fmt.Println("У вас избыточный вес!")
-	default:
-		fmt.Println("Ты жирдяй!")
+	`) // Мультистроки
+	for {
+		usrHeight, usrKg := getUserInput()
+		IMT := calculateIMT(usrKg, usrHeight)
+		outputResult(IMT)
+		isRepeateCalc := checkRepeatCalculation()
+		if isRepeateCalc {
+			break
+		}
 	}
-	outputResult(IMT)
 }
 
 func outputResult(imt float64) {
 	result := fmt.Sprintf("Индекс массы тела: %.0f", imt)
 	fmt.Print(result)
+	switch {
+	case imt < 16:
+		fmt.Println("\nУ вас сильный дефицит массы тела!")
+	case imt < 18.5:
+		fmt.Println("\nУ вас дефицит массы тела!")
+	case imt < 25:
+		fmt.Println("\nУ вас нормальный вес!")
+	case imt < 30:
+		fmt.Println("\nУ вас избыточный вес!")
+	default:
+		fmt.Println("\nТы жирдяй!")
+	}
 }
 
 func calculateIMT(userKg, userHeight float64) float64 {
@@ -47,4 +51,11 @@ func getUserInput() (userHeight float64, userKg float64) {
 	fmt.Println("Введите свой вес в киллограммах")
 	fmt.Scan(&userKg)
 	return
+}
+
+func checkRepeatCalculation() bool {
+	var answer string
+	fmt.Println("\nХотите сделать расчет еще раз?(да/нет)\n*****************************")
+	fmt.Scan(&answer)
+	return answer == "нет"
 }
